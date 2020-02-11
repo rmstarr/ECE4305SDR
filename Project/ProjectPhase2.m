5b4fafb976379f43b56f6d303e0185c68926b61c
Robbie Starr <42650446+rmstarr@users.noreply.github.com>
2020-02-11 17:07:44 -0500

    need to implement GMSK filtering and Demod

--- Project/ProjectPhase2.m
+++ Project/ProjectPhase2.m
@@ -1,42 +1,36 @@
 %% Project Phase 2: Computer Simulation
 % Ethan, Andrew, and Robbie
+clear all;
+visuals = true;
 
 %% System Parameters
-preamble = 01010101; % 1 byte
+preamble = [0 1 0 1 0 1 0 1]; % 1 byte
 accAddr = 'A8C8F245'; % 4 bytes
 PDUlength = 257; % amount of data in bytes
-CRC = 0; % will be defined based on what data ends up as
+CRClength = 3; % will be defined based on what data ends up as
 
 PDUbits = PDUlength*8;
 rawData = ones(1, PDUbits); % generation of "raw" data
+CRCbits = CRClength*8;
+CRC = zeros(1, CRCbits);
 
 accAddrBinary = hexToBinaryVector(accAddr);
 
 dataPacket = [preamble accAddrBinary rawData CRC];
 
-
-
-
-
-
-
-
-
-
-
-
-
-%% Lab 1 Part 2: Fine Frequency Correction
-
-% Debugging flags
-visuals = true;
+% turn to column vector
+dataPacket = dataPacket';
 
 %% General system details
 sampleRateHz = 1e6; % Sample rate
 samplesPerSymbol = 1;
-frameSize = 2^10;
-numFrames = 10;
-numSamples = numFrames*frameSize; % Samples to simulate
+frameSize = 2^3;
+numSamples = 2097; % Samples to simulate
+modulationOrder = 2;
+filterUpsample = 4;
+filterSymbolSpan = 8;
+inputSamplesPerSymbol = 4;      % Input samples in a single symbol
+decimationFactor = 2;
 
 %% Fine Frequency Compensator Variable initialization:
 
@@ -73,8 +67,8 @@ cdPreOut = comm.ConstellationDiagram('ReferenceConstellation', [-1 1],...
 
 %% Impairments
 snr = 15;
-frequencyOffsetHz = sampleRateHz*0.02; % Offset in hertz
-phaseOffset = 0; % Radians
+% frequencyOffsetHz = sampleRateHz*0.02; % Offset in hertz
+% phaseOffset = 0; % Radians
 
 %% Generate symbols
 modulatedData = mod.step(dataPacket);
@@ -83,12 +77,18 @@ modulatedData = mod.step(dataPacket);
 noisyData = awgn(modulatedData,snr);%,'measured');
 
 
-%% Define Communication Object
+%% Define Communication Object - will change to GMSK (including Gaussian filter)
 
     fineSync = comm.CarrierSynchronizer('DampingFactor',lamda, ...
     'NormalizedLoopBandwidth',loopBand, ...
     'SamplesPerSymbol',samplesPerSymbol, ...
     'Modulation','QPSK');
+
+%% Digital Downsampling and Filtering
+% Raised cosine filter to restructure samples
+rcRxFilt = comm.RaisedCosineReceiveFilter('InputSamplesPerSymbol', inputSamplesPerSymbol, 'FilterSpanInSymbols', filterSymbolSpan, 'DecimationFactor', decimationFactor);
+filteredData = step(rcRxFilt, noisyData);
+
 %% Model of error
 % Add frequency offset to baseband signal
 
@@ -113,19 +113,40 @@ for k=1:frameSize:numSamples
     % Plot it
     
     Phase(k:k+frameSize-1) = phaseOff(timeIndex);
-    if visuals
-        step(cdPre,noisyData(timeIndex));
-        step(cdPre,offsetData(timeIndex));
-        step(cdPost,rxData(timeIndex));pause(0.1); %#ok<*UNRCH>
-    end
+%     if visuals
+%         step(cdPre,noisyData(timeIndex));
+%         step(cdPre,offsetData(timeIndex));
+%         step(cdPost,rxData(timeIndex));pause(0.1); %#ok<*UNRCH>
+%     end
 
 end
-figure;
-plot(phaseOff)
-estFreqOffset = diff(phaseOff)*sampleRateHz/(2*pi);
-rmean = cumsum(estFreqOffset)./(1:length(estFreqOffset))';
-plot(rmean)
-xlabel('Symbols')
-ylabel('Estimated Frequency Offset (Hz)')
-title('Estimate of Frequency Offset: damping = 1/sqrt((2), loop BW = 0.05')
-grid
+% figure;
+% plot(phaseOff)
+% estFreqOffset = diff(phaseOff)*sampleRateHz/(2*pi);
+% rmean = cumsum(estFreqOffset)./(1:length(estFreqOffset))';
+% plot(rmean)
+% xlabel('Symbols')
+% ylabel('Estimated Frequency Offset (Hz)')
+% title('Estimate of Frequency Offset: damping = 1/sqrt((2), loop BW = 0.05')
+% grid
+
+%% Where Synchronization would go - don't need it
+
+%% Demodulation
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
