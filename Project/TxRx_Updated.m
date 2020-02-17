178c2172831885dce9c63815dea7c2333841003f
ADuncanBruin <53100591+ADuncanBruin@users.noreply.github.com>
2020-02-17 16:01:30 -0500

    Newest File

--- Project/TxRx_Updated.m
+++ Project/TxRx_Updated.m
@@ -9,7 +9,7 @@ visuals = true;
 
 %% General system details
 sampleRateHz = 1e6;                     % Sample rate
-samplesPerSymbol = 1;
+samplesPerSymbol = 8;
 frameSize = 2^3;                        % Size of data frame (1 byte)
 modulationOrder = 2;
 filterUpsample = 4;                     % Upsampling factor
@@ -46,120 +46,158 @@ DATA_NO_HEADER = [rawData CRC]';
 numSamples = length(DATA_NO_HEADER);                      % Samples to simulate
 
 %% Impairments
-snr = 15;
-% frequencyOffsetHz = sampleRateHz*0.02; % Offset in hertz
-% phaseOffset = 0; % Radians
-
-% %% Generate symbols   - Probably don't need, GF/MSK modulation happens in
-% bleTX
-% modulatedData = mod.step(dataPacket);
-
+% Iterate across a range of SNR values
+snr = 0:1:20;
 
 %% Transmit the Data in BLE
 bleTx = bleWaveformGenerator(DATA_NO_HEADER, 'Mode', BLE_Mode, 'ChannelIndex', channel,...
     'SamplesPerSymbol', samplesPerSymbol, 'AccessAddress', accAddrBinary);
 
-
-%% Raised Cosine Filter
-
-
-TxFlt = comm.RaisedCosineTransmitFilter('OutputSamplesPerSymbol', filterUpsample, 'FilterSpanInSymbols', filterSymbolSpan);
-filteredData = step(TxFlt, bleTx);
 %% Add RF Imparements to signal to recover 
+for i = 1:length(snr)
+    % send through awgn channel
+    noisyData = awgn(bleTx,snr(i));%,'measured');
 
-% send through awgn channel
-noisyData = awgn(filteredData,snr);%,'measured');
+    % Apply Frequency Offset
+    frequencyOffsetHz = 10000;
+    % Add frequency offset to noisy data.
+    normalizedOffset = 1i.*2*pi*frequencyOffsetHz./sampleRateHz;
+    offsetData = zeros(size(noisyData));
+    for k=1:frameSize:numSamples
 
-% Apply Frequency Offset
-frequencyOffsetHz = 10000;
-% Add frequency offset to noisy data.
-normalizedOffset = 1i.*2*pi*frequencyOffsetHz./sampleRateHz;
-offsetData = zeros(size(noisyData));
-for k=1:frameSize:numSamples
-    
-    timeIndex = (k:k+frameSize-1).';
-    freqShift = exp(normalizedOffset*timeIndex);
-    
-    % Offset data and maintain phase between frames
-    offsetData(timeIndex) = noisyData(timeIndex).*freqShift;
+        timeIndex = (k:k+frameSize-1).';
+        freqShift = exp(normalizedOffset*timeIndex);
+
+        % Offset data and maintain phase between frames
+        offsetData(timeIndex) = noisyData(timeIndex).*freqShift;
+    end
+    %% Steps to Receiver:
+
+    % 1. Automatic gain control (AGC) 
+    % 2. DC removal 
+    % 3. Carrier frequency offset correction 
+    % 4. Matched filtering
+    % 5. Packet detection
+    % 6. Timing error correction
+    % 7. Demodulation and decoding
+    % 8. Dewhitening
+
+    % Initialize Receiver Objects and Variables:
+    % Bluetooth Object 
+    phyMode = 'LE1M';
+    bleParam = helperBLEReceiverConfig(phyMode);
+
+    %AGC
+    agc = comm.AGC('MaxPowerGain',20,'DesiredOutputPower',2);
+
+    % FFC
+    loopBand = 0.05; % Loop bandwidt
+    lamda = 1 / sqrt(2) ; % Dampening Factor
+    fineSync = comm.CarrierSynchronizer('DampingFactor',lamda, ...
+        'NormalizedLoopBandwidth',loopBand, ...
+        'SamplesPerSymbol',samplesPerSymbol, ...
+        'Modulation','QPSK');
+
+    %% Automatic Gain Control
+    % Increase gain of transmitted BLE signal
+    agcData = agc(bleTx);
+
+    %% DC removal
+    % Subtract the mean from the signal.
+    dcData = agcData - mean(agcData);
+
+    %% Carrier Frequency Offset Correction (Our implementation)
+    freqAdjust = fineSync(dcData);
+
+    %% Everything Else
+    prbDet = comm.PreambleDetector(bleParam.RefSeq,'Detections','First');
+    % Initialize counter variables
+    pktCnt = 0;
+    crcCnt = 0;
+    displayFlag = true; % true if the received data is to be printed
+
+    % Loop to decode the captured BLE samples
+    while length(dataCaptures) > bleParam.MinimumPacketLen
+
+        % Consider two frames from the captured signal for each iteration
+        startIndex = 1;
+        endIndex = min(length(dataCaptures),2*bleParam.FrameLength);
+        rcvSig = dataCaptures(startIndex:endIndex);
+
+        rcvAGC = agc(rcvSig); % Perform AGC
+        rcvDCFree = rcvAGC - mean(rcvAGC); % Remove the DC offset
+        rcvFreqComp = freqCompensator(rcvDCFree); % Estimate and compensate for the carrier frequency offset
+        rcvFilt = conv(rcvFreqComp,bleParam.h,'same'); % Perform gaussian matched filtering
+
+        % Perform frame timing synchronization
+        [~, dtMt] = prbDet(rcvFilt);
+        release(prbDet)
+        prbDet.Threshold = max(dtMt);
+        prbIdx = prbDet(rcvFilt);
+
+        % Extract message information
+        [cfgLLAdv,pktCnt,crcCnt,remStartIdx] = helperBLEPhyBitRecover(rcvFilt,...
+                                    prbIdx,pktCnt,crcCnt,bleParam);
+
+        % Remaining signal in the burst captures
+        dataCaptures = dataCaptures(1+remStartIdx:end);
+
+        % Display the decoded information
+        if displayFlag && ~isempty(cfgLLAdv)
+            fprintf('Advertising PDU Type: %s\n', cfgLLAdv.PDUType);
+            fprintf('Advertising Address: %s\n', cfgLLAdv.AdvertiserAddress);
+        end
+
+        % Release System objects
+        release(freqCompensator)
+        release(prbDet)
+    end
+
+    % Release the signal source
+    release(sigSrc)
+
+    % Determine the PER
+    if pktCnt
+        per(i) = 1-(crcCnt/pktCnt);
+        fprintf('Packet error rate for %s mode is %f.\n',bleParam.Mode,per);
+    else
+        fprintf('\n No BLE packets were detected.\n')
+    end
+
 end
-%% Steps to Receiver:
+%% Data Display
 
-% 1. Automatic gain control (AGC) 
-% 2. DC removal 
-% 3. Carrier frequency offset correction 
-% 4. Matched filtering
-% 5. Packet detection
-% 6. Timing error correction
-% 7. Demodulation and decoding
-% 8. Dewhitening
+% Plot of SNR versus PER 
+plot(snr, per)
+title('PER Versus SNR')
+xlabel('SNR (dB)')
+ylabel('PER')
 
 
-% Initialize Receiver Objects and Variables:
-
-
-% Bluetooth Object 
-phyMode = 'LE1M';
-bleParam = helperBLEReceiverConfig(phyMode);
-
-%AGC
-agc = comm.AGC('MaxPowerGain',20,'DesiredOutputPower',2);
-
-% FFC
-loopBand = 0.05; % Loop bandwidt
-lamda = 1 / sqrt(2) ; % Dampening Factor
-fineSync = comm.CarrierSynchronizer('DampingFactor',lamda, ...
-    'NormalizedLoopBandwidth',loopBand, ...
-    'SamplesPerSymbol',samplesPerSymbol, ...
-    'Modulation','QPSK');
-preDet = comm.PreambleDetector(bleParam.RefSeq,'Detections','First');
-
-% Initialize counter variables
-pktCnt = 0;
-crcCnt = 0;
-displayFlag = true; % true if the received data is to be printed
-
-
-%% Automatic Gain Control
-agcData = agc(offsetData);
-
-%% DC removal
-% Subtract the mean from the signal.
-dcData = agcData - mean(agcData);
-%% Match Filtering:
-% Raised cosine filter to restructure samples
-rcRxFilt = comm.RaisedCosineReceiveFilter('InputSamplesPerSymbol', inputSamplesPerSymbol, 'FilterSpanInSymbols', filterSymbolSpan, 'DecimationFactor', decimationFactor);
-filteredData = step(rcRxFilt, dcData);
-
-%% Carrier Frequency Offset Correction (Our implementation)
-freqAdjust = fineSync(filteredData);
+%% Stuff Removed
+% %% Gaussian Match Filtering:
 % 
-
-%% Timing Synchronization:
-
-% Not gonna do, we'll have this complete when we do the thing
-% Haven't done yet. Can we find a way to do it without actually
-% implementing it on our own? We'll take a look at 
-
-% Perform frame timing synchronization
-
-% Code from example. Don't know what it does, but it contains information
-% we need to complete the receiver structure. 
-
-%% Demodulation
+% rcvFilt = conv(freqAdjust, bleParam.h, 'same');
+% %% Timing Synchronization:
 % 
-gDemod = comm.GMSKDemodulator('BandwidthTimeProduct', 0.5, 'PulseLength', 1, 'SamplesPerSymbol', samplesPerSymbol);
-extractedData = gDemod(filteredData); % Demodulate the Data
-%% Encoding, (Demodulation), Dewhittening, and extraction of Data:
-
-% NOTE: WHEN IMPLEMENTING, DOUBLE CHECK THAT YOU DO DEMODULATION ONLY ONCE.
-% CURRENTLY THE FUNCTION HERE DOES DEMODULATION. SO BEFORE RUNNING THE
-% PROGRAM, DELETE ONE OF THOSE. 
-[dewhitenedBits,pktCnt,crcCnt,startIdx] = helperBLEPhyBitRecover_Data_No_Time_Comp(extractedData,pktCnt,crcCnt,bleParam);
-
-
-% Release Receiver Objects:
-
-release(preDet);
-release(fineSync);
-release(agc);
+% % Perform frame timing synchronization
+% 
+% [~, mt] = prbDet(rcvFilt);
+% %disp(mt);
+% release(prbDet);
+% prbDet.Threshold = max(mt);
+% prbInd = prbDet(rcvFilt);
+% 
+% %% Demodulation
+% 
+% 
+% %% Encoding, (Demodulation), Dewhittening, and extraction of Data:
+% 
+% %[bits, accessAddress] = bleReceiverPractical(gDemod,bleParam, channel, initRxParams);
+% [cfgllData,pktCnt,crcCnt,startIdx] = dataBLEPhyBitRecover(rcvFilt, prbInd,pktCnt,crcCnt,bleParam);
+% 
+% % Release Receiver Objects:
+% disp(cfgllData)
+% 
+% release(fineSync);
+% release(agc);
