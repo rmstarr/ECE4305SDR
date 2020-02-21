f518d00660c9f40a3c28e2db85d19cde62cc2c3a
Robbie Starr <42650446+rmstarr@users.noreply.github.com>
2020-02-21 11:04:52 -0500

    Add files via upload

---
+++ Rxx.m
@@ -0,0 +1,90 @@
+%% General parameters
+sampleRateHz = 1e6;             % Sample Rate in Hz
+visuals = true;                 % Used for displaying constellation
+filterSymbolSpan = 10;           % Number of symbols filter spans
+decimationFactor = 1;           % Downsampling factor
+GainFactor = 30;                % Gain of received signal
+SamplesPerFrame = 4096;         % Samples per frame of data         
+inputSamplesPerSymbol = 4;     % Input samples in a single symbol
+
+%% Receiver setup
+Rx = sdrrx('Pluto', 'SamplesPerFrame', SamplesPerFrame, 'OutputDataType', 'double', 'GainSource', 'Manual', 'Gain', GainFactor);
+Rx.BasebandSampleRate = sampleRateHz;
+
+%% Digital Downsampling and Filtering
+% Raised cosine filter to restructure samples
+rcRxFilt = comm.RaisedCosineReceiveFilter('InputSamplesPerSymbol', inputSamplesPerSymbol, 'FilterSpanInSymbols', filterSymbolSpan, 'DecimationFactor', decimationFactor);
+% filteredData = step(rcRxFilt, inputData);
+
+%% Define Communication Object
+fineSync = comm.CarrierSynchronizer('DampingFactor',1/sqrt(2), ...
+'NormalizedLoopBandwidth',0.01, ...
+'SamplesPerSymbol',1, ...
+'Modulation','QPSK');
+
+%% Timing synchronization object setup
+timeSyncPLUTO = comm.SymbolSynchronizer('Modulation', 'PAM/PSK/QAM', ...
+   'NormalizedLoopBandwidth', ...
+    0.01);
+
+
+%% Setup Constellation Object for Visualization
+cdPre = comm.ConstellationDiagram('ReferenceConstellation', [-1 1],...
+    'Name','Input');
+cdPost = comm.ConstellationDiagram('ReferenceConstellation', [-1 1],...
+    'SymbolsToDisplaySource','Property',...
+    'SymbolsToDisplay',SamplesPerFrame/2,...
+    'Name','Output');
+cdPre.Position(1) = 50;
+cdPost.Position(1) = cdPre.Position(1)+cdPre.Position(3)+10;% Place side by side
+ap = dsp.ArrayPlot;ap.ShowGrid = true;
+ap.Title = 'Frequency Histogram';ap.XLabel = 'Hz';ap.YLabel = 'Magnitude';
+ap.XOffset = -sampleRateHz/2;
+ap.SampleIncrement = (sampleRateHz)/(2^10);
+cdOut = comm.ConstellationDiagram('ReferenceConstellation', [-1 1],...
+    'Name','Baseband');
+cdPreOut = comm.ConstellationDiagram('ReferenceConstellation', [-1 1],...
+    'Name','Baseband');
+
+%% Model Received and Fine Frequency Compensated Data
+constPoints = 4; % DBPSK, but same constellation as 2-PAM
+refConst = qammod(data, constPoints);
+evm = comm.EVM('ReferenceSignalSource', 'Estimated from reference constellation', ...
+    'ReferenceConstellation', refConst);
+RxFilteredEVMdata = [];
+RxFilteredCorrectedEVMdata = [];
+
+while 1
+    % Receive Data from Tx
+    inputData = Rx();  
+    
+    % Fine Frequency Compensation
+    receivedData = fineSync(inputData);
+    raisedCos = rcRxFilt(receivedData);
+    time = timeSyncPLUTO(raisedCos);
+    
+    rxFilterEVM = evm(receivedData);
+    rxTCorrectedEVM = evm(time);
+    
+    RxFilteredEVMdata = [RxFilteredEVMdata; rxFilterEVM];
+    RxFilteredCorrectedEVMdata = [RxFilteredCorrectedEVMdata; rxTCorrectedEVM];
+
+    % Display constellations
+    if visuals
+        step(cdPre,receivedData);
+        step(cdPost,time);
+        pause(0.1);
+    end
+    
+release(cdPost);
+end
+
+%% EVM Analysis of the Signals
+% constPoints = 4; % DBPSK, but same constellation as 2-PAM
+% refConst = qammod(data, constPoints);
+% evm = comm.EVM('ReferenceSignalSource', 'Estimated from reference constellation', ...
+%     'ReferenceConstellation', refConst);
+% 
+% rxFilterEVM = evm(receivedFilteredData);
+% 
+% rxTCorrectedEVM = evm(receivedTCorrectedData);
---
+++ TxRx.m
@@ -0,0 +1,154 @@
+%% Timing Correction on PLUTO SDR
+% Ethan Martin, Robert Starr, and Andrew Duncan
+
+%% Communication with Timing Correction on a Single PLUTO
+%% General system details
+sampleRateHz = 1e6;                     % Sample rate
+samplesPerSymbol = 4;                   % Upsampling factor    
+decimationFactor = 2;                   % Downsampling factor
+frameSize = 2^5;                       % Frame size
+numFrames = 15;                        % Total frames to simulate
+numSamples = numFrames*frameSize;       % Samples to simulate
+modulationOrder = 2;                    % Modulation order of signal
+filterSymbolSpan = 10;                  % Span of Filter
+normalizedLoopBandwidth = 0.01;         % Normalized Loop BW of RC filter
+
+%% Visuals
+% Constellations
+cdPre = comm.ConstellationDiagram('ReferenceConstellation', [-1 1],...
+    'Name','Baseband');
+cdPost = comm.ConstellationDiagram('ReferenceConstellation', [-1 1],...
+    'SymbolsToDisplaySource','Property',...
+    'SymbolsToDisplay',frameSize/2,...
+    'Name','Baseband with Timing Offset');
+cdPre.Position(1) = 50;
+cdPost.Position(1) = cdPre.Position(1)+cdPre.Position(3)+10;% Place side by side
+
+% EVM Scope
+scope = dsp.TimeScope('YLabel', 'EVM (%)', 'SampleRate', 1000, 'TimeSpan', 10);
+
+%% PLUTO Radio setup
+% Declaration of system parameters for hardware testing
+centerFrequency = 2.4e9;
+
+% Creation of SDR TX and RX objects
+% Transmitter
+transmitter = sdrtx('Pluto', ...
+    'CenterFrequency', centerFrequency, ...
+    'Gain', 0); 
+
+% Receiver
+receiver = sdrrx('Pluto', ...
+    'CenterFrequency', centerFrequency, ...
+    'SamplesPerFrame', 1e6, ...
+    'OutputDataType', 'double');
+
+%% Data creation
+data = randi([0 modulationOrder-1], numSamples*2, 1);
+mod = comm.DBPSKModulator();
+modulatedData = mod.step(data);
+phaseOffset = 30;
+
+%% Transmit and receiver filter setup
+txFilterPLUTO = comm.RaisedCosineTransmitFilter('OutputSamplesPerSymbol', ...
+    samplesPerSymbol, 'FilterSpanInSymbols', filterSymbolSpan);
+
+rxFilterPLUTO = comm.RaisedCosineReceiveFilter('InputSamplesPerSymbol', ...
+    samplesPerSymbol, 'FilterSpanInSymbols', filterSymbolSpan, ... 
+    'DecimationFactor', decimationFactor );
+
+%% Impairments and channel creation
+snr = 15;
+timingOffset = samplesPerSymbol*0.01; % Samples
+
+%% Frequency Synchronization Object:
+
+freqSync = comm.CarrierSynchronizer('Modulation', 'BPSK', 'SamplesPerSymbol', samplesPerSymbol, 'DampingFactor', 1/sqrt(2));
+
+%% Timing synchronization object setup
+timeSyncPLUTO = comm.SymbolSynchronizer('Modulation', 'PAM/PSK/QAM', ...
+    'SamplesPerSymbol', samplesPerSymbol,'NormalizedLoopBandwidth', ...
+    normalizedLoopBandwidth);
+
+%% Delay factor
+tDelay = dsp.VariableFractionalDelay();
+
+%% Throw Data into transmit raised cosine:
+
+% Filter Signal (TX Side) 
+filteredTxData = step(txFilterPLUTO, modulatedData);
+
+%% Creation of Signal, TX Offset, and TX Corrected Signal
+filteredRxData = [];
+tCorrectedSig = [];
+
+RxFilteredEVMdata = [];
+RxFilteredCorrectedEVMdata = [];
+
+tscope = dsp.TimeScope('YLabel', 'EVM (%)', 'YLimits', [0 40], 'SampleRate', 1000, 'TimeSpan', 1.2, 'ShowGrid', true);
+
+frameCount = 0;
+while frameCount < numFrames
+    
+    % Continuously transmit the data repeatedly
+    transmitter.transmitRepeat(filteredTxData);
+    
+    % Continuosly receiving the data
+    receivedData = receiver();
+    
+    % Carrier Synchronization:
+    alignedData = step(freqSync,receivedData);
+    
+    %     % Filter Signal (RX Side)
+    filteredRxData = step(rxFilterPLUTO, alignedData);
+    %
+    % Do Timing Correction
+    tCorrectedSig = step(timeSyncPLUTO, filteredRxData);
+    
+    %% EVM Analysis of the Signals
+    constPoints = 2; % DBPSK, but same constellation as 2-PAM
+    refConst = pammod(data, constPoints);
+    evm = comm.EVM('ReferenceSignalSource', 'Estimated from reference constellation', ...
+        'ReferenceConstellation', refConst);
+    
+    rxFilterEVM = evm(filteredRxData);
+    rxTCorrectedEVM = evm(tCorrectedSig);
+    
+    %tscope(rxTCorrectedEVM);
+    RxFilteredEVMdata = [RxFilteredEVMdata; rxFilterEVM];
+    RxFilteredCorrectedEVMdata = [RxFilteredCorrectedEVMdata; rxTCorrectedEVM];
+    
+    % Visualize Error
+    step(cdPre, receivedData);
+    step(cdPost, tCorrectedSig);
+    pause(0.1);
+    release(cdPost)
+    
+    frameCount = frameCount + 1;
+end
+
+%% EVM data graph
+
+
+
+
+
+% % Release the objects, transmit the corrected data
+% release(transmitter);
+% release(receiver);
+% 
+% transmitter(tCorrectedSig);
+% receivedTCorrectedData = receiver();
+% 
+% release(transmitter);
+% release(receiver);
+
+%% EVM Analysis of the Signals
+% constPoints = 2; % DBPSK, but same constellation as 2-PAM
+% refConst = pammod(data, constPoints);
+% evm = comm.EVM('ReferenceSignalSource', 'Estimated from reference constellation', ...
+%     'ReferenceConstellation', refConst);
+% 
+% rxFilterEVM = evm(receivedFilteredData);
+% 
+% rxTCorrectedEVM = evm(receivedTCorrectedData);
