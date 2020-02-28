7db1d5441906175226424e5d7e73fd5b0d3147ef
ADuncanBruin <53100591+ADuncanBruin@users.noreply.github.com>
2020-02-28 11:28:45 -0500

    Returns CRC packet, finds all preambles.

--- BLE_CRC_Preamble_Blocks.m
+++ BLE_CRC_Preamble_Blocks.m
@@ -18,80 +18,38 @@ crcGen = comm.CRCGenerator('Polynomial', CRC_poly, 'InitialConditions', 0, ...
     'DirectMethod', true);
 crcDet = comm.CRCDetector('Polynomial', CRC_poly, 'InitialConditions', 0, ...
     'DirectMethod', true);
-fullPacket = crcGen(packet);
-
-%% Cross correlation (Theory)
-
-% Find the preamble (and spacing), given that we know what to look for
-preamble2Find = [0 1 0 1 0 1 0 1]';
-gapLength = length(fullPacket);  
-
-% Perform cross-correlation, find where preamble is
-[corr, lag] = xcorr(fullPacket, preamble2Find);
-L = length(corr);
-[~, index] = max(corr);      % Determine the index of highest xcorr
-
-% Plot of correlation results
-stem(lag, corr);
-xlim([0 length(fullPacket)]);
-    
+bleDataPacket = crcGen(packet);    
 
 %% Using comm.PreambleDetector to Find End of BLE Preamble
 tic
 % Preamble to find
 blePreamble = [0 1 0 1 0 1 0 1]';
-
-% Generate a block of 2112 random data points (everything besides preamble)
-randDat = randi([0 1], 2112, 1);
-
-% Make a "packet" of simulated data
-packet = [blePreamble; randDat];
+packets2Collect = 100;
 
 % Repeat the data sequence - simulate collection of multiple packets in
 % buffer (Pretending to collect 100 packets)
-bigPacket = repmat(packet, 100, 1);
+bigPacket = repmat(bleDataPacket, packets2Collect, 1);
 
 % Creation of Preamble Detector Object - find the Preamble from collected
 % data (Note: Set to detect each 
-prbDet = comm.PreambleDetector(blePreamble, 'Input', 'Bit', 'Detections', 'All');
+prbDet = comm.PreambleDetector(blePreamble, 'Input', 'Bit', 'Detections', 'First');
 
-% Find END of preamble
-prbEnd = prbDet(bigPacket);
+% Find END of FIRST preamble
+firstPreamble = prbDet(bigPacket);
+
+% Verify that packets are aligned by preambles
+packetLength = length(packet);
+allPreambles = firstPreamble:packetLength:length(bigPacket);
+
+% Check to verify that all of these are truly preambles (extra)
+for i = 1:length(allPreambles)
+    preambleCheck = bigPacket(allPreambles(i) - (length(blePreamble) - 1):allPreambles(i));
+    if preambleCheck ~= blePreamble
+        disp("Error in preamble")
+    end
+end
 
 toc % Done just to see how fast process is
 
-%% Creation of Multiple Packets - Apply Cross Correlation to find Preamble
 
-
-% Logic:
-%   - Take in multiple packets (say, 100 packets)
-%   - Take the xcorr of each packet
-%   - Save the indices fron which maximum xcorr achieved
-%   - As multiple iterations occur, take note of where spikes are. The
-%     spikes should be consistent. If there are discrepancies, eliminate
-%     the index from contention
-
-% Preamble to find for LE1M and LE2M
-preambleLE1M = [0 1 0 1 0 1 0 1]';
-
-% Instantiation of maxval arrays
-lastMax = [];
-currMax = [];
-
-% Try taking in 100 packets - create new data each time
-for iteration = 1:100
-    
-    % Creation of new data packet
-    randomPDU = randi([0 1], (PDULength)*bytes2bits, 1);
-    BLEPacket = [preamble; accAddrBinary; randomPDU];
-
-    % Perform cross correlation, find indices or maximum correlation
-    corr = xcorr(BLEPacket, preambleLE1M);
-    [~, index] = max(corr);
-    
-    % Set indices of maximum correlation in currMax, then compare to
-    % indices in lastMax. Only keep indices that are in both the new and
-    % old max arrays
-    
-    
-end
+%% Processing of Data (Once it Has Been Received)
